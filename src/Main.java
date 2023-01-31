96d99c4a394a4186a8f1380bf5a694988dfd21cf
Sergey Lehmann <sxm21anapa@gmail.com>
2023-01-31 21:16:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialAccount = 100;
+        int add = 1100;
+        int bonus = 0;
+        if (add > 1000) {
+            bonus = add / 100;
+        }
+        int finalAccount = initialAccount + add + bonus;
+
+        System.out.println("Итоговый счёт:" + finalAccount);
+        System.out.println("Бонус:" + bonus);
+    }
+}
